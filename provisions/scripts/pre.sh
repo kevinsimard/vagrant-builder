9302c4834939645200e6c08818ef22b12fe3b877
Kevin Simard <kev.simard@gmail.com>
2014-07-22 12:23:25 -0400

    Add essential packages to pre provision file

--- provisions/scripts/pre.sh
+++ provisions/scripts/pre.sh
@@ -2,3 +2,10 @@
 
 # update packages
 apt-get update
+
+# upgrade packages
+apt-get upgrade -y
+
+# install essential packages
+apt-get install -y make automake autotools openssl\
+    git-core screen curl build-essential htop unzip zip
