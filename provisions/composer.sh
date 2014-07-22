e36f7aa3dbde6ca1ada1feae6fdda56e0e83f998
Kevin Simard <kev.simard@gmail.com>
2014-07-22 13:53:02 -0400

    Add commands to composer provision file

--- provisions/composer.sh
+++ provisions/composer.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+if ! type -p php > /dev/null; then
+    # download and install composer.phar to /usr/bin directory
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin
+
+    # create a symlink from composer.phar to composer
+    ln -s /usr/bin/composer.phar /usr/bin/composer
+fi
