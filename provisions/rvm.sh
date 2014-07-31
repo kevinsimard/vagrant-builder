c52b27a2f1429981048cf85c8675d2ca9ab0d1b2
Kevin Simard <kev.simard@gmail.com>
2014-07-31 11:53:32 -0400

    Add commands to rvm provision file

--- provisions/rvm.sh
+++ provisions/rvm.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+if ! type -p rvm > /dev/null; then
+    # updating Ruby Version Manager
+    rvm get stable --ignore-dotfiles
+else
+    # install RVM and install latest stable Ruby version
+    curl -sSL https://get.rvm.io | bash -s stable --ruby
+fi
