1abdec0ad9fe4337762df00dd67616aa3fda78f4
Kevin Simard <kev.simard@gmail.com>
2014-07-31 11:50:56 -0400

    Add requirements to composer provision file

--- provisions/composer.sh
+++ provisions/composer.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-if ! type -p php > /dev/null; then
-    # download and install composer.phar to /usr/bin directory
-    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin
-
-    # create a symlink from composer.phar to composer
-    ln -s /usr/bin/composer.phar /usr/bin/composer
+# install requirements
+if type -p php > /dev/null; then
+    sh php-cli.sh
 fi
+
+# download and install composer.phar to /usr/bin directory
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin
+
+# create a symlink from composer.phar to composer
+ln -s /usr/bin/composer.phar /usr/bin/composer
