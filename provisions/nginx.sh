440ae58e0ecdc7e5df1f5c0e5d1f40d30577f3c4
Kevin Simard <kev.simard@gmail.com>
2014-08-01 15:22:18 -0400

    Add commands to nginx provision file

--- provisions/nginx.sh
+++ provisions/nginx.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+
+# install Nginx
+apt-get install -y nginx
+
+# turn off sendfile to be more compatible with Windows
+sed -i "s/sendfile on;/sendfile off;/" /etc/nginx/nginx.conf
+
+# set set hash bucket size to 64
+sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
+
+# restart Nginx service
+service nginx restart
