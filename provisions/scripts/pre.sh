564a39cf5b78adda3531ca0bb8a130bf23bd8eb8
Kevin Simard <kev.simard@gmail.com>
2014-08-01 10:40:18 -0400

    Add vagrant user to www-data group

--- provisions/scripts/pre.sh
+++ provisions/scripts/pre.sh
@@ -9,3 +9,6 @@ apt-get upgrade -y
 # install essential packages
 apt-get install -y make automake openssl screen\
     git-core curl build-essential htop unzip zip
+
+# add vagrant user to www-data group
+usermod -a -G www-data vagrant
