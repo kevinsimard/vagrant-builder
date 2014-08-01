d25a5711534241c34e435b88bafdfe1f156b51c2
Kevin Simard <kev.simard@gmail.com>
2014-08-01 11:30:33 -0400

    Check if php is installed before installing

--- provisions/composer.sh
+++ provisions/composer.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-# install requirements
-if ! which php > /dev/null; then
-    sh ./php.sh
+if which php > /dev/null; then
+    # download and install Composer to /usr/bin directory
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin
+
+    # create a symlink from composer.phar to composer
+    ln -s /usr/bin/composer.phar /usr/bin/composer
 fi
-
-# download and install Composer to /usr/bin directory
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin
-
-# create a symlink from composer.phar to composer
-ln -s /usr/bin/composer.phar /usr/bin/composer
