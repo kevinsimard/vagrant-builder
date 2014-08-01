a856a89e58661c0dad1c7b943bc5ccf8bc01a782
Kevin Simard <kev.simard@gmail.com>
2014-08-01 15:21:03 -0400

    Add commands to mysql provision file

--- provisions/mysql.sh
+++ provisions/mysql.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+
+# install MySQL server
+apt-get install -y mysql-server
+
+# set username and password of MySQL to 'root'
+debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+
+# enable remote access to MySQL from everywhere
+sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+
+# grant privileges to root user from everywhere
+MYSQL=`which mysql`
+Q1="GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;"
+Q2="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}"
+$MYSQL -uroot -proot -e "$SQL"
+
+# restart MySQL service
+service mysql restart
