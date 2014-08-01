00ad5317d1432f8b71b905d07ec9103112e387c4
Kevin Simard <kev.simard@gmail.com>
2014-08-01 13:31:33 -0400

    Add commands to haproxy provision file

--- provisions/haproxy.sh
+++ provisions/haproxy.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+# install HAProxy
+apt-get install -y haproxy
+
+# enable HAProxy via init script
+sed -i "s/ENABLED=0/ENABLED=1/" /etc/default/haproxy
+
+# start HAProxy service
+service haproxy start
