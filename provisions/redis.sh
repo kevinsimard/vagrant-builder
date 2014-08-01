c9f66e5a000c5518c85b45e7602cd1d23808f635
Kevin Simard <kev.simard@gmail.com>
2014-08-01 13:25:24 -0400

    Add commands to redis provision file

--- provisions/redis.sh
+++ provisions/redis.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+
+# install Redis server
+apt-get install -y redis-server
+
+# add persistent config
+tee -a /etc/redis/redis.conf << EOF
+appendonly yes
+appendfsync everysec
+EOF
+
+# schedule background append rewriting
+if ! crontab -l | grep -q "redis-cli bgrewriteaof"; then
+    line="*/5 * * * * /usr/bin/redis-cli bgrewriteaof > /dev/null 2>&1"
+    (crontab -l; echo "$line" ) | crontab -
+fi
+
+# restart Redis server
+service redis-server restart
