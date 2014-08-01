2ebc34608b64b37be55d80facab11358760b943f
Kevin Simard <kev.simard@gmail.com>
2014-08-01 15:21:28 -0400

    Add commands to mongodb provision file

--- provisions/mongodb.sh
+++ provisions/mongodb.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+
+# add MongoDB key to sources
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" |\
+    tee /etc/apt/sources.list.d/mongodb.list
+
+# update packages
+apt-get update
+
+# install MongoDB server
+apt-get install -y mongodb-org
+
+if which php > /dev/null; then
+    # install PHP extension
+    apt-get -y install php5-mongo
+
+    # restart PHP5-FPM service
+    service php5-fpm restart
+fi
