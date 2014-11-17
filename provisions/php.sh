b5676ac5ae3601f4a5b3f672594a3edaf4c8cd26
Kevin Simard <kev.simard@gmail.com>
2014-11-17 08:17:12 -0500

    Remove condition that checks if php was already installed

--- provisions/php.sh
+++ provisions/php.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-if ! which php > /dev/null; then
-    # install PHP5-CLI/FPM
-    echo -e "\e[1;34mInstalling PHP5...\e[0m"
-    apt-get install -qq php5-cli php5-fpm
+# install PHP5-CLI/FPM
+echo -e "\e[1;34mInstalling PHP5...\e[0m"
+apt-get install -qq php5-cli php5-fpm
 
-    # install PHP5 extensions
-    echo -e "\e[1;34mInstalling PHP5 extensions...\e[0m"
-    apt-get install -qq php5-mysql php5-pgsql php5-sqlite php5-dev\
-        php5-curl php5-gd php5-mcrypt php5-xdebug php5-memcached
+# install PHP5 extensions
+echo -e "\e[1;34mInstalling PHP5 extensions...\e[0m"
+apt-get install -qq php5-mysql php5-pgsql php5-sqlite php5-dev\
+    php5-curl php5-gd php5-mcrypt php5-xdebug php5-memcached
 
-    # set PHP5-FPM to listen on TCP instead of Socket
-    sed -i "s/listen =.*/listen = 127.0.0.1:9000/" /etc/php5/fpm/pool.d/www.conf
+# set PHP5-FPM to listen on TCP instead of Socket
+sed -i "s/listen =.*/listen = 127.0.0.1:9000/" /etc/php5/fpm/pool.d/www.conf
 
-    # set xdebug config
-    cat > $(find /etc/php5 -name xdebug.ini) << EOF
+# set xdebug config
+cat > $(find /etc/php5 -name xdebug.ini) << EOF
 zend_extension=$(find /usr/lib/php5 -name xdebug.so)
 xdebug.remote_enable = 1
 xdebug.remote_connect_back = 1
@@ -23,6 +22,3 @@ xdebug.scream=0
 xdebug.cli_color=1
 xdebug.show_local_vars=1
 EOF
-else
-    echo -e "\e[1;34mWARNING: PHP is already installed!\e[0m"
-fi
