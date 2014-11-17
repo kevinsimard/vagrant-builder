57aa936ce428fc9a380c9904cb94a1e69a9dd6c3
Kevin Simard <kev.simard@gmail.com>
2014-11-17 08:16:01 -0500

    Fix typo

--- provisions/nginx.sh
+++ provisions/nginx.sh
@@ -7,7 +7,7 @@ apt-get install -qq nginx
 # turn off sendfile to be more compatible with Windows
 sed -i "s/sendfile on;/sendfile off;/" /etc/nginx/nginx.conf
 
-# set set hash bucket size to 64
+# set hash bucket size to 64
 sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
 
 # clean default config files
