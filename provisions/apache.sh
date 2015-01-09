7248d15e95e2f8559a440d54370c9abf5899060e
Kevin Simard <kev.simard@gmail.com>
2015-01-09 14:12:43 -0500

    Add php5 mods to apache2

--- provisions/apache.sh
+++ provisions/apache.sh
@@ -2,7 +2,7 @@
 
 # install Apache
 echo -e '\e[1;34mInstalling Apache2...\e[0m'
-apt-get install -qq apache2
+apt-get install -qq apache2 libapache2-mod-php5
 
 # clean default config files
 rm /etc/apache2/sites-enabled/*
